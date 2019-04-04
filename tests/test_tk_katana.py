6c98bec922073a664ff1bb5aa031299b2c13e635
Joseph Yu <joseph.yu@wwfx.co.uk>
2019-04-04 11:04:52 +0100

    Removed rst files

---
+++ tests/test_tk_katana.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `tk_katana` package."""
+
+
+import unittest
+
+from tk_katana import tk_katana
+
+
+class TestTk_katana(unittest.TestCase):
+    """Tests for `tk_katana` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
