8fee4511280af4bd8d6dae3b491ae965a2f93df2
Joseph Yu <joseph.yu@wwfx.co.uk>
2019-07-15 11:33:40 +0100

    Debugging build output

--- docs/conf.py
+++ docs/conf.py
@@ -171,9 +171,9 @@ html_static_path = ['_static']
 # You may only specify the root package of the
 # dependencies themselves and omit the sub-modules:
 autodoc_mock_imports = [
-    "sgtk", "tank",
-    "Katana", "UI4",
-    "Qt",
+    # "sgtk", "tank",               # Shotgun imports
+    "Katana", "AssetAPI", "UI4",  # Katana imports
+    # "Qt",                         # Qt imports
 ]
 
 # -- Options for todo extension ----------------------------------------------
